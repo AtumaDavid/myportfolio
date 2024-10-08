f177a88cd070006f9262751f77e5313028aa4d30
Atuma David <atumadavid35@gmail.com>
2024-10-08 17:36:59 +0100

    minor changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import MagicButton from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa";
 import { socialMedia } from "@/data";
+import Link from "next/link";
 
 export default function Footer() {
   return (
@@ -27,7 +28,7 @@ export default function Footer() {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="mailto:contact@jsmastery.pro">
+        <a href="mailto:atumadavid35@gmail.com">
           <MagicButton
             title="Send an Email, Let's get in touch"
             icon={<FaLocationArrow />}
@@ -42,12 +43,14 @@ export default function Footer() {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <div
+            <Link
+              href={info.link}
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
               <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
+              {/* <Link href={info.link} /> */}
+            </Link>
           ))}
         </div>
       </div>
--- data/index.ts
+++ data/index.ts
@@ -213,13 +213,16 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/AtumaDavid",
   },
-  {
-    id: 2,
-    img: "/twit.svg",
-  },
+  // {
+  //   id: 2,
+  //   img: "/twit.svg",
+  //   link: "https://www.linkedin.com/in/ikechukwu-atuma/",
+  // },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/ikechukwu-atuma/",
   },
 ];
